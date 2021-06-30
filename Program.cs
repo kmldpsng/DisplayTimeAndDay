bf0516beb5f9419ceadf4441d47318ab0281b1a9
Kamal <31344909+kmldpsng@users.noreply.github.com>
2021-06-30 12:43:52 +0530

    Required Changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace DisplayTimeAndDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var unformatteddate = System.DateTime.Now;
+
+            var date = string.Format("{0:dddd, MMMM d, yyyy}", unformatteddate);
+
+            var unformattedtime = System.DateTime.Now;
+
+            var time = string.Format("{0:T}", unformattedtime);
+
+            Console.WriteLine(date + "\n" + time);
+
+            Console.ReadLine();
         }
     }
 }
